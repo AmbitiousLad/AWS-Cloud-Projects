e045ee4a7bacbafcb4808041798cae9729272db4
Sanket <100010175+AmbitiousLad@users.noreply.github.com>
2024-12-13 17:20:10 +0530

    Add files via upload

---
+++ Multi-tier-app-deployment/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'three-tier-rds.c1kqeyuisvgw.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Password123',
+    DB_DATABASE : 'webappdb'
+});
